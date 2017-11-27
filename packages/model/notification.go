896bafc6b325cd9c48aa4856f0258ef3e022c3c3
Evgeny Lerner <dvork1ng.egaas@gmail.com>
2017-11-27 18:39:19 +0300

    notification logic changed

--- packages/model/notification.go
+++ packages/model/notification.go
@@ -4,13 +4,13 @@ import (
 	"strconv"
 )
 
-func GetAllNotifications(ecosystemID int64, lastNotificationID int64, userIDs []int64) ([]map[string]string, error) {
-	query := `select * from "` + strconv.FormatInt(ecosystemID, 10) +
-		`_notifications" where closed = 0 and id > ` + strconv.FormatInt(lastNotificationID, 10) +
-		` and recipient_id in (`
+func GetNotificationsCount(ecosystemID int64, userIDs []int64) ([]map[string]string, error) {
+	query := `select count(recipient_id), max(id), recipient_id from "` + strconv.FormatInt(ecosystemID, 10) +
+		`_notifications" where recipient_id in (`
 	for _, userID := range userIDs {
 		query += strconv.FormatInt(int64(userID), 10) + ", "
 	}
-	query = query[:len(query)-2] + ");"
+	query = query[:len(query)-2] + `) group by "recipient_id";`
+
 	return GetAll(query, -1)
 }
--- packages/notificator/notificator.go
+++ packages/notificator/notificator.go
@@ -2,6 +2,7 @@ package notificator
 
 import (
 	"encoding/json"
+	"fmt"
 	"strconv"
 
 	"github.com/AplaProject/go-apla/packages/consts"
@@ -12,10 +13,17 @@ import (
 
 type EcosystemID int64
 type UserID int64
+type NotificationID int64
 
 type NotificationStats struct {
-	UserIDs     map[UserID]int64
-	lastNotifID *int64
+	UserIDs     map[UserID]NotificationID
+	lastNotifID *NotificationID
+}
+
+type notificationRecord struct {
+	RecipientID  UserID
+	MaxNotifID   NotificationID
+	RecordsCount int64
 }
 
 var notifications map[EcosystemID]NotificationStats
@@ -24,21 +32,22 @@ func init() {
 	notifications = make(map[EcosystemID]NotificationStats)
 }
 
+func AddUser(userID int64, ecosystemID int64) {
+	if _, ok := notifications[EcosystemID(ecosystemID)]; !ok {
+		notifications[EcosystemID(ecosystemID)] = NotificationStats{UserIDs: make(map[UserID]NotificationID), lastNotifID: new(NotificationID)}
+	}
+	notifications[EcosystemID(ecosystemID)].UserIDs[UserID(userID)] = 0
+}
+
 func SendNotifications() {
 	for ecosystemID, ecosystemStats := range notifications {
-		notifs := getEcosystemNotifications(ecosystemID, *ecosystemStats.lastNotifID, ecosystemStats)
+		notifs := mapToStruct(getEcosystemNotifications(ecosystemID, ecosystemStats))
 		for _, notif := range notifs {
-			userID, err := strconv.ParseInt(notif["recipient_id"], 10, 64)
-			if err != nil {
-				log.WithFields(log.Fields{"type": consts.ConvertionError, "value": notif["recipient_id"], "error": err}).Error("getting recipient_id")
-				return
+			if notifications[ecosystemID].UserIDs[notif.RecipientID] >= notif.MaxNotifID {
+				continue
 			}
-			data, err := mapToString(notif)
-			if err != nil {
-				log.WithFields(log.Fields{"type": consts.MarshallingError, "error": err}).Error("marshalling notification")
-				return
-			}
-			ok, err := publisher.Write(userID, data)
+			ok, err := publisher.Write(int64(notif.RecipientID), notif.String())
+
 			if err != nil {
 				log.WithFields(log.Fields{"type": consts.IOError, "error": err}).Error("writing to centrifugo")
 				return
@@ -48,10 +57,7 @@ func SendNotifications() {
 				log.WithFields(log.Fields{"type": consts.CentrifugoError, "error": err}).Error("writing to centrifugo")
 				return
 			}
-			id, _ := strconv.ParseInt(notif["id"], 10, 64)
-			if *notifications[ecosystemID].lastNotifID < id {
-				*notifications[ecosystemID].lastNotifID = id
-			}
+			notifications[ecosystemID].UserIDs[notif.RecipientID] = notif.MaxNotifID
 		}
 	}
 }
@@ -64,24 +70,64 @@ func mapToString(value map[string]string) (string, error) {
 	return string(bytes), nil
 }
 
-func getEcosystemNotifications(ecosystemID EcosystemID, lastNotificationID int64, userIDs NotificationStats) []map[string]string {
+func getEcosystemNotifications(ecosystemID EcosystemID, userIDs NotificationStats) []map[string]string {
 	users := make([]int64, 0)
 	for userID := range userIDs.UserIDs {
 		users = append(users, int64(userID))
 	}
-	rows, err := model.GetAllNotifications(int64(ecosystemID), lastNotificationID, users)
+	rows, err := model.GetNotificationsCount(int64(ecosystemID), users)
 	if err != nil || len(rows) == 0 {
 		if err != nil {
-			log.WithFields(log.Fields{"type": consts.DBError, "error": err}).Error("getting all notifications")
+			log.WithFields(log.Fields{"type": consts.DBError, "error": err}).Error("getting notifications count")
 		}
 		return nil
 	}
 	return rows
 }
 
-func AddUser(userID int64, ecosystemID int64) {
-	if _, ok := notifications[EcosystemID(ecosystemID)]; !ok {
-		notifications[EcosystemID(ecosystemID)] = NotificationStats{UserIDs: make(map[UserID]int64), lastNotifID: new(int64)}
+func mapToStruct(dbData []map[string]string) []notificationRecord {
+	var result []notificationRecord
+	for _, record := range dbData {
+		nf := &notificationRecord{}
+		err := nf.ParseMap(record)
+		if err != nil {
+			continue
+		}
+		result = append(result, *nf)
 	}
-	notifications[EcosystemID(ecosystemID)].UserIDs[UserID(userID)] = 0
+	return result
+}
+
+func (nr *notificationRecord) ParseMap(data map[string]string) error {
+	convert := func(value string, errMessage string) (int64, error) {
+		var result int64
+		result, err := strconv.ParseInt(data[value], 10, 64)
+		if err != nil {
+			log.WithFields(log.Fields{"type": consts.ConvertionError, "error": err}).Error(errMessage)
+		}
+		return result, err
+	}
+
+	maxID, err := convert("max", "error converting max id")
+	if err != nil {
+		return err
+	}
+
+	count, err := convert("count", "error converting records count")
+	if err != nil {
+		return err
+	}
+
+	userID, err := convert("recipient_id", "error converting records count")
+	if err != nil {
+		return err
+	}
+	nr.MaxNotifID = NotificationID(maxID)
+	nr.RecordsCount = count
+	nr.RecipientID = UserID(userID)
+	return nil
+}
+
+func (nr *notificationRecord) String() string {
+	return fmt.Sprintf(`{"count": %d}`, nr.RecordsCount)
 }
